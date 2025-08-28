895cc9f65f3ffb8a9a73d36394e0b4c1fdb6b43c
Chintan Ghate <chintanghate@gmail.com>
2025-08-28 10:12:17 +0530

    Add Ugandan image phrases

--- lib/src/repos/phrases_repository.dart
+++ lib/src/repos/phrases_repository.dart
@@ -69,6 +69,7 @@ final class PhrasesRepository extends ChangeNotifier {
     }
     updated = true;
     final languagePackList = [
+       // LanguagePackSummary(version: 'draft', name: 'Ugandan images', language: NaturalLanguage.fromCodeShort('en'), languagePackCode: 'en.ugandan-images', phrasesCount: 51),
       // LanguagePackSummary(version: 'draft', name: 'Ugandan english', language: NaturalLanguage.fromCodeShort('en'), languagePackCode: 'en.ugandan-english', phrasesCount: 200),
       // LanguagePackSummary(version: 'draft', name: 'Luganda text prompts', language: NaturalLanguage.fromCodeShort('lg'), languagePackCode: 'lg.luganda-text-prompts', phrasesCount: 210),
       // LanguagePackSummary(version: 'draft', name: 'Ugandan luo text phrases', language: NaturalLanguage.fromCodeShort('lg'), languagePackCode: 'lg.ugandan-luo-text-prompts', phrasesCount: 200),
