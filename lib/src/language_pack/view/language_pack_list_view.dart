565f7d8f89cb7b8ce3216440ea209234bf4acf28
Chintan Ghate <chintanghate@gmail.com>
2025-11-07 20:51:55 +0530

    Implement showAddLanguagePackDialog in language_pack_list_view.

--- lib/src/language_pack/view/language_pack_list_view.dart
+++ lib/src/language_pack/view/language_pack_list_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sealed_languages/sealed_languages.dart';
 
 import '../../common/result.dart';
 import '../language_pack_list_tile.dart';
+import '../model/language_pack.dart';
 import '../model/language_pack_catalog_model.dart';
 import '../model/language_pack_summary.dart';
 import '../repository/language_pack_repo.dart';
@@ -15,12 +17,22 @@ class LanguagePackListView extends StatefulWidget {
 }
 
 class _LanguagePackListViewState extends State<LanguagePackListView> {
+  late LanguagePackRepository _languagePackRepository;
+  NaturalLanguage? _selectedLanguage;
+  final TextEditingController _languageFilterController =
+      TextEditingController();
+  final TextEditingController _nameFieldController = TextEditingController();
+  final TextEditingController _codeFieldController = TextEditingController();
+  String _warningMessage = '';
+  String _languagePackCode = '';
 
   @override
   void initState() {
-    LanguagePackRepository repo = context.read();
+    _languagePackRepository = context.read();
     LanguagePackCatalogModel model = context.read();
-    repo.getLanguagePackSummaryList().then((Result<List> languagePackListResult) {
+    _languagePackRepository
+        .getLanguagePackSummaryList()
+        .then((Result<List> languagePackListResult) {
       List<dynamic> fetchedResults = [];
       switch (languagePackListResult) {
         case Ok<List>():
@@ -31,15 +43,150 @@ class _LanguagePackListViewState extends State<LanguagePackListView> {
       }
       List<LanguagePackSummary> mappedList = fetchedResults
           .map((x) => LanguagePackSummary(
-          version: x.version,
-          name: x.name,
-          language: x.language,
-          languagePackCode: x.languagePackCode,
-          phrasesCount: x.phrasesCount))
+              version: x.version,
+              name: x.name,
+              language: x.language,
+              languagePackCode: x.languagePackCode,
+              phrasesCount: x.phrasesCount))
           .toList();
-      model.updateLanguagePackSummaryList(List<LanguagePackSummary>.from(mappedList));
+      model.updateLanguagePackSummaryList(
+          List<LanguagePackSummary>.from(mappedList));
     });
     super.initState();
+  }
+
+  // TODO (chintanghate@gmail.com): Refactor this.
+  void _showAddLanguagePackDialog() {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        final listOfAllLanguages = NaturalLanguage.list;
+        return StatefulBuilder(
+            builder: (context, StateSetter setStateInsideDialog) => AlertDialog(
+                  icon: Icon(Icons.language,
+                      color: Theme.of(context).colorScheme.primary),
+                  title: const Text('Create a language pack'),
+                  content: Wrap(runSpacing: 16, children: [
+                    DropdownMenu<NaturalLanguage>(
+                        controller: _languageFilterController,
+                        enableFilter: true,
+                        enableSearch: true,
+                        menuHeight: 300,
+                        requestFocusOnTap: true,
+                        leadingIcon: const Icon(Icons.search),
+                        label: const Text('Language'),
+                        dropdownMenuEntries: listOfAllLanguages
+                            .map((l) =>
+                                DropdownMenuEntry(label: l.name, value: l))
+                            .toList(),
+                        onSelected: (selectedLang) => setStateInsideDialog(() {
+                              _selectedLanguage = selectedLang;
+                              if (_selectedLanguage != null &&
+                                  _nameFieldController.text.isNotEmpty) {
+                                _languagePackCode =
+                                    "${_selectedLanguage?.codeShort.toLowerCase() ?? ""}.${_nameFieldController.text.toLowerCase().split(' ').join('-')}";
+                                _warningMessage = "";
+                              }
+                            }),
+                        filterCallback:
+                            (List<DropdownMenuEntry<NaturalLanguage>> entries,
+                                String filter) {
+                          final String trimmedFilter =
+                              filter.trim().toLowerCase();
+                          if (trimmedFilter.isEmpty) {
+                            return entries;
+                          }
+                          return entries
+                              .where(
+                                (DropdownMenuEntry<NaturalLanguage> entry) =>
+                                    entry.label
+                                        .toLowerCase()
+                                        .contains(trimmedFilter),
+                              )
+                              .toList();
+                        },
+                        searchCallback:
+                            (List<DropdownMenuEntry<NaturalLanguage>> entries,
+                                String query) {
+                          if (query.isEmpty) {
+                            return null;
+                          }
+                          final int index = entries.indexWhere(
+                              (DropdownMenuEntry<NaturalLanguage> entry) =>
+                                  entry.label == query);
+                          return index != -1 ? index : null;
+                        }),
+                    TextField(
+                      controller: _nameFieldController,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Pack name',
+                          hintText: 'Daily Phrases'),
+                      onChanged: (val) {
+                        setStateInsideDialog(() {
+                          if (_selectedLanguage != null) {
+                            _languagePackCode =
+                                "${_selectedLanguage?.codeShort.toLowerCase() ?? ""}.${val.toLowerCase().split(' ').join('-')}";
+                            _warningMessage = "";
+                          } else {
+                            _codeFieldController.text = '';
+                          }
+                        });
+                      },
+                    ),
+                    Text(_languagePackCode),
+                    Text(_warningMessage,
+                        style: Theme.of(context).textTheme.bodyMedium)
+                  ]),
+                  actions: <Widget>[
+                    TextButton(
+                      child: const Text('Cancel'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    TextButton(
+                      child: const Text('Save'),
+                      onPressed: () {
+                        setStateInsideDialog(() {
+                          if (_selectedLanguage == null) {
+                            _warningMessage =
+                                'Please select a language to create a pack';
+                          } else if (_nameFieldController.text.isEmpty) {
+                            _warningMessage =
+                                'Please enter name to create a pack';
+                          }
+                        });
+                        setState(() {
+                          if (_selectedLanguage != null &&
+                              _languagePackCode.isNotEmpty) {
+                            final newLanguagePack = LanguagePack(
+                                version: 'draft',
+                                name: _nameFieldController.text,
+                                language: _selectedLanguage!,
+                                phrases: []);
+                            _languagePackRepository.addLanguagePack(
+                                languagePack: newLanguagePack);
+                            _selectedLanguage = null;
+                            _nameFieldController.text = "";
+                            Navigator.of(context).pop();
+                            LanguagePackCatalogModel model = context.read();
+                            var updatedLanguagePackSummaryList =
+                                model.languagePackSummaryList;
+                            updatedLanguagePackSummaryList.add(
+                                LanguagePackSummary.fromJson(
+                                    newLanguagePack.toSummaryJson()));
+                            model.updateLanguagePackSummaryList(
+                                updatedLanguagePackSummaryList);
+                          }
+                        });
+                      },
+                    ),
+                  ],
+                ));
+      },
+    );
   }
 
   @override
@@ -61,9 +208,10 @@ class _LanguagePackListViewState extends State<LanguagePackListView> {
           SliverPadding(padding: EdgeInsets.symmetric(vertical: 16)),
           SliverToBoxAdapter(child: SizedBox(height: 100))
         ]),
-        floatingActionButton:
-            FloatingActionButton(child: Icon(Icons.add), onPressed: () {
-              print("ADD A NEW LANGUAGE PACK");
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              _showAddLanguagePackDialog();
             }));
   }
 }
